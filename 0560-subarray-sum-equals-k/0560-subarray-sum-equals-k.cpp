5a8443c3d1733c0785d0c97d567b33931bc146b8
Sarah Abraham <44671982+sarahab23@users.noreply.github.com>
2024-04-07 18:09:41 +0530

    Time: 64 ms (70.78%), Space: 50.7 MB (19.52%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -6,7 +6,7 @@ public:
         prefSumCount[0] = 1;
        for(int i = 0; i < size; i++){
             prefSum += nums[i];
-            result = result + prefSumCount[prefSum - k]; 
+            result += prefSumCount[prefSum - k]; 
             prefSumCount[prefSum]++;
         }
         return result;
