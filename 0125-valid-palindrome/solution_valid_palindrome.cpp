71bcf7fad8f0ce8d67cb011851847c0c9d93e598
Sarah Abraham <44671982+sarahab23@users.noreply.github.com>
2024-04-26 17:30:16 +0530

    Update and rename best_solution_valid_palindrome.cpp to solution_valid_palindrome.cpp
    Solution using in built functions

--- 0125-valid-palindrome/solution_valid_palindrome.cpp
+++ 0125-valid-palindrome/solution_valid_palindrome.cpp
@@ -1,12 +1,10 @@
-// Solution without using inbuilt functions
 class Solution {
 public:
     bool isPalindrome(string s) {
         string newString = "";
         for(char c:s){
-            if((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122)){
-                if(c >= 65 && c <= 90) newString += c + 32;
-                else newString += tolower(c);
+            if(isalnum(c)){
+                newString += tolower(c);
             }
         }
         int size = newString.size();
