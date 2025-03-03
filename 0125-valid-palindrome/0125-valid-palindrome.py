27acbdc18079115871ac501a9960aa398a9c5b4d
Sarah Abraham <44671982+sarahab23@users.noreply.github.com>
2025-03-03 14:14:24 +0530

    Time: 27 ms (5.02%), Space: 18.1 MB (57.69%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,16 @@
 class Solution:
+    def alphaNum(self, c):
+            return (ord('0') <= ord(c) <= ord('9')) or (ord('A') <= ord(c) <= ord('Z')) or (ord('a') <= ord(c) <= ord('z'))
+            
     def isPalindrome(self, s: str) -> bool:
         low = 0
         high = len(s) - 1
 
         while low < high:
-            if not s[low].isalnum():
+            if not self.alphaNum(s[low]):
                 low += 1
                 continue
-            if not s[high].isalnum():
+            if not self.alphaNum(s[high]):
                 high -= 1
                 continue
             if s[low].lower() == s[high].lower():
