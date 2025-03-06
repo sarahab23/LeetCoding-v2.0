f779ccd9277733764b025b83b62dcc5920573666
Sarah Abraham <44671982+sarahab23@users.noreply.github.com>
2025-03-06 14:35:26 +0530

    Time: 0 ms (100%), Space: 18.6 MB (73.59%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -2,7 +2,7 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, h = 0, len(nums) - 1
         while l <= h:
-            mid = l + ((h - l)// 2) # or (l + h)//2
+            mid = l + ((h - l)// 2) # or (l + h)//2 because if l & h is too high, like close to a 32 bit number, then sum of l + h cannot be calculated, this is for other languages like C++/ java
             if nums[mid] > target: h = mid - 1
             elif nums[mid] < target: l = mid + 1
             else: return mid
