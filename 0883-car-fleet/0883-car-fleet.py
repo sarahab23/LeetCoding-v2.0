8cf2e7257c8e6deab56c7f106f98018598fd36b8
Sarah Abraham <44671982+sarahab23@users.noreply.github.com>
2025-03-07 17:15:33 +0530

    Time: 151 ms (74.88%), Space: 36.9 MB (94.25%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,17 +1,11 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        n = len(position)
-        posSpd = [] # pairs of [pos, speed]
         res = 0
         prevTime = 0
+        pairs = zip(position, speed)
 
-        for i in range(n):
-            posSpd.append([position[i], speed[i]])
-        
-        posSpd.sort(key=lambda x: x[0], reverse=True) # Sort in descending order of position
-
-        for i in range(n):
-            time = (target - posSpd[i][0])/posSpd[i][1] 
+        for p,s in sorted(pairs, reverse = True):
+            time = (target - p)/s
             if time > prevTime: 
                 res += 1
                 prevTime = time
