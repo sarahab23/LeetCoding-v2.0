dbdf18375b5e5df9f22ec4bd014770a41252ef47
Sarah Abraham <44671982+sarahab23@users.noreply.github.com>
2025-03-07 16:11:25 +0530

    Time: 191 ms (39.35%), Space: 39 MB (51.71%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,15 +1,16 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        n = len(position)
         stack = [] # stack of time
-        posSpd = []
+        posSpd = [] # pairs of [pos, speed]
         res = 0
 
-        for i in range(len(position)):
+        for i in range(n):
             posSpd.append([position[i], speed[i]])
         
-        posSpd.sort(key=lambda x: x[0], reverse=True)
+        posSpd.sort(key=lambda x: x[0], reverse=True) # Sort in descending order of position
 
-        for i in range(len(position)):
+        for i in range(n):
             time = (target - posSpd[i][0])/posSpd[i][1]
             if not stack: stack.append(time)
             else:
@@ -18,4 +19,4 @@ class Solution:
                 if not stack: res += 1
                 stack.append(time)
         
-        return res + 1 if stack else res
+        return res + 1
