988d45f9f1b9ac9db991ef7a3e05e6a15d80f476
Sarah Abraham <44671982+sarahab23@users.noreply.github.com>
2025-03-07 17:17:17 +0530

    Time: 140 ms (85.83%), Space: 38.4 MB (55.48%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,10 +1,10 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         res = 0
-        prevTime = 0
-        pairs = zip(position, speed)
+        prevTime = 0 # storing slow prev car's time
+        pairs = zip(position, speed) # pairs are iterators of tuples 
 
-        for p,s in sorted(pairs, reverse = True):
+        for p,s in sorted(pairs)[::-1]:
             time = (target - p)/s
             if time > prevTime: 
                 res += 1
