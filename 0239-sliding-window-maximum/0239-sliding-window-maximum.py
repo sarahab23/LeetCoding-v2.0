6936a11359f223f24e7f470b5edbf9c234e7775f
Sarah Abraham <44671982+sarahab23@users.noreply.github.com>
2025-03-09 17:12:54 +0530

    Time: 163 ms (68.28%), Space: 30.8 MB (93.02%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -4,7 +4,7 @@ class Solution:
         q = collections.deque()
 
         for i in range(0,k):
-            if q: 
+            if q: # making sure max no. is placed first, followed by decreasing no.s
                 while q and nums[q[-1]] < nums[i]: q.pop()
             q.append(i)
 
@@ -12,8 +12,8 @@ class Solution:
 
         l, r = 1, k
         while r < len(nums):
-            if (l - 1) == q[0]: q.popleft()
-            while q and nums[q[-1]] < nums[r]: q.pop()
+            if (l - 1) == q[0]: q.popleft() # removing prev l
+            while q and nums[q[-1]] < nums[r]: q.pop() # adding r
             q.append(r)
             r += 1
             l += 1
