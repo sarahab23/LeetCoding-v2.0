1c91c48e5fed32f4c4db2f4a79c83a148d3f48da
Sarah Abraham <44671982+sarahab23@users.noreply.github.com>
2025-03-23 19:37:34 +0530

    Update and rename 0789-kth-largest-element-in-a-stream.py to 0789-kth-largest-element-in-a-stream_better.py

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream_better.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream_better.py
@@ -2,6 +2,10 @@ import heapq
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
+        # Worst case would be using Array D.S, sorting array in constructor (TC = O(NlogN)) & in add() you'd do Binary Search + Add new elt - worst case the new 
+        # elt goes in the middle, so you have to shift n/2 elts, TC = O(N)
+        # Current method, we're using heap. [Adding N elts + Popping N - k elts] Constructor TC = O(NlogN) + O(N - k)(logN)
+        # Add() - TC = O(logN)
         self.k = k
         heapq.heapify(nums)
         while len(nums) > k:
